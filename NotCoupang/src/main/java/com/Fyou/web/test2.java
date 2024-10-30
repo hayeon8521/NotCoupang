dc2a38fc0782f79da62ff708ac2a43841bba1f8d
Hayeon <Hayeon@DESKTOP-F0MAH1V>
2024-10-31 03:32:49 +0900

    adfas

--- NotCoupang/src/main/java/com/Fyou/web/test2.java
+++ NotCoupang/src/main/java/com/Fyou/web/test2.java
@@ -12,17 +12,7 @@ public class test2 implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		// BuyerTM/프론트컨트롤선언파일명.tiles
-		// BuyerTM/test2.do => BuyerTM/test2.tiles
-		
-		//헤드만 필요한 페이지 호출할때 (BuyerTM)
-		//req.getRequestDispatcher("BuyerTM/test2.tiles").forward(req, resp);
-		
-		//해드와 사이드바가 필요할때 (BuyerTAM)
-		//req.getRequestDispatcher("BuyerTAM/test2.tiles").forward(req, resp);
-		
-		//관리자 페잊지
+		// TODO Auto-generated method stub
 		req.getRequestDispatcher("sellerAM/test2.tiles").forward(req, resp);
 	}
 
