4684fff2edd739c68277a4df462a11b94bf402ef
admin <admin@DESKTOP-E8F35B4>
2024-10-31 11:27:26 +0900

    ㅌㅋㅊㅋㅌㅊ

--- NotCoupang/src/test/java/com/Fyou/test/AppTest.java
+++ NotCoupang/src/test/java/com/Fyou/test/AppTest.java
@@ -1,8 +1,13 @@
 package com.Fyou.test;
 
+import com.Fyou.service.CategoriService;
+import com.Fyou.service.CategoriServiceImpl;
 
 public class AppTest {
 	public static void main(String[] args) {
+		//CategoriService svc = new CategoriServiceImpl();
+		
+		
 		
 	}
 }
