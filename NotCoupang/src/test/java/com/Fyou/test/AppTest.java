22efc661449fbe9908958539ae7d8b413369e7b7
admin <admin@DESKTOP-C14DAE7>
2024-10-31 09:17:58 +0900

    sdfsf

--- NotCoupang/src/test/java/com/Fyou/test/AppTest.java
+++ NotCoupang/src/test/java/com/Fyou/test/AppTest.java
@@ -9,11 +9,25 @@ public class AppTest {
 		
 		MemberService svc = new MemberServiceImpl();
 		
-		String m_id = "test";
+		String m_id = "hayeon";
 		
-		MemberVO member = svc.selectMember(m_id);
+		MemberVO member = new MemberVO();
+		member.setMemberId("hayeon");
+		member.setMemberPw("1234");
+		member.setMemberNick("하욘");
+		member.setMemberName("표하연");
+		member.setMemberPhone("010-8521-6704");
+		member.setMemberAddress("대구시 여러분 왕초입니다");
+		member.setMemberDivision("buyer");
 		
-		System.out.println("a");
-		System.out.println(member.toString());
+		if(svc.addMember(member)) {
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+		
+		MemberVO mb = svc.selectMember(m_id);
+		
+		System.out.println(mb.toString());
 	}
 }
