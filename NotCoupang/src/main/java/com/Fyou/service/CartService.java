b81a5952bae24da33f2ac08efb52f72e553ec85c
seojin <admin@DESKTOP-U820E4V>
2024-10-31 10:01:46 +0900

    2024.10.31 장바구니 집에서 작업한거

--- NotCoupang/src/main/java/com/Fyou/service/CartService.java
+++ NotCoupang/src/main/java/com/Fyou/service/CartService.java
@@ -5,19 +5,20 @@ import java.util.List;
 import com.Fyou.vo.CartVO;
 
 public interface CartService {
-	//카트 조회
+	
+	//카트 전체 목록조회
 	List<CartVO> cartList();
 	
 	//카트 조회(로그인 id별)
 	public List<CartVO> selectCart(String memberId);
 	
 	//카트 추가
-	boolean insertCart(String memberId, int goodsNum);
+	boolean insertCart(CartVO cart);
 	
 	//카트 삭제
-
-	boolean deleteCart(String memberId, int seqCart);
-
+	boolean deleteCart(CartVO cart);
+	
+	//카트 수정
 	boolean updateCart(CartVO cart);
 	
 	
