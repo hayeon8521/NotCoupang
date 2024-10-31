f764477a58329888c6384d4fbed91c0a0593cea2
admin <admin@DESKTOP-FMDOL73>
2024-10-31 09:45:25 +0900

    2024.10.31 001

--- NotCoupang/src/main/java/com/Fyou/service/AnswerService.java
+++ NotCoupang/src/main/java/com/Fyou/service/AnswerService.java
@@ -6,7 +6,7 @@ import com.Fyou.vo.AnswerVO;
 import com.Fyou.vo.AskVO;
 
 public interface AnswerService {
-	public List<AskVO> selectAskList(String member_id);
+	public List<AskVO> selectAskList(String member_id, int page);
 	public AnswerVO selectAnswer(int seq_ask);
 	public boolean deleteAsk(int seq_ask);
 	public boolean insertAsk(AskVO ask);
